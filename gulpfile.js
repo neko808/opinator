39b7c9e4f5ed00939b21bacaab0370112b02f4e8
Alex <extrem303@gmail.com>
2024-10-25 13:22:04 -0600

    Adden new gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+// const sass = require('gulp-sass');
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+
+//compile sccs into css
+function style() {
+  // 1. where is my scss file
+  return (
+    gulp
+      .src("./scss/*.scss")
+
+      // 2. pass that file through sass compiler
+      .pipe(sass())
+      //.on('error', sass.logError))
+      // 3. where do I save the compíled css?
+      .pipe(gulp.dest("./css"))
+      // 4. stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+
+  gulp.watch("./scss/*.scss", style);
+  gulp.watch("./js/*.js").on("change", browserSync.reload);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
